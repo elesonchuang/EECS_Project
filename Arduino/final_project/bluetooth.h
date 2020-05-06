33193f5c106d1330b9182db2a4e66f70d5aa54da
fundwotsai2001 <B08901204@ntu.edu.tw>
2020-05-06 11:42:30 +0800

    merge

--- Arduino/final_project/bluetooth.h
+++ Arduino/final_project/bluetooth.h
@@ -56,7 +56,7 @@ BT_CMD ask_BT(){
 // (but need to convert to byte type)
 void send_msg(const char& msg)
 {if (digitalRead(l1) == HIGH&& digitalRead(l2) == HIGH &&digitalRead(l3) == HIGH &&  digitalRead(r1) == HIGH &&digitalRead(r2) == HIGH && digitalRead(r3) == HIGH){ 
- BT.write("H")；}
+ BT.write('H')；}
          
               
 }// send_msg
